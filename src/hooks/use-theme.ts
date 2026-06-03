b1781687ba2f7f98e7c461f99d7158a4401ff1c1
DILTD\thhoang <thhoang@diltd.com.vn>
2026-06-03 10:32:59 +0700

    change theme to darkmode default

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -4,7 +4,7 @@ export function useTheme() {
   const [dark, setDark] = useState<boolean>(() => {
     const stored = localStorage.getItem('theme')
     if (stored) return stored === 'dark'
-    return window.matchMedia('(prefers-color-scheme: dark)').matches
+    return true
   })
 
   useEffect(() => {
